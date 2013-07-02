4b85dad528cfa3df65e68e3fd64e5dcd6e0ff331
Matt Lynch <njdevil@gmail.com>
2013-07-02 17:44:06 -0400

    Fixed a bug on views.py that was preventing both comments & voting to occur on the same page. Fixed index.html accordingly.

--- restaurant/views.py
+++ restaurant/views.py
@@ -34,30 +34,37 @@ def index(request):
 
 
 def show_restaurant(request,restaurant):
-    _detail="x"
+    _detail="xxx"
     data=Restaurant.objects.filter(slug=restaurant).values().extra(select={'commentcount':'select count(restaurant_comment.id) from restaurant_comment where restaurant_comment.restaurant_id=restaurant_restaurant.id'},)
     commentnameid=data[0]["id"]
     comments=Comment.objects.filter(restaurant__name=data[0]["name"])
-    content=request.POST.get('postcontent','')
-    content=re.sub("<a href=.*?</a>","",content)
-    content=re.sub("<\?","",content)
+#    content=request.POST.get('postcontent','')
+#    content=re.sub("<a href=.*?</a>","",content)
+#    content=re.sub("<\?","",content)
+
+ #   if content:
+ #       Comment.objects.create(restaurant_id=commentnameid, user_id=request.user.id, content=content).save()
 
     if request.POST:
         post=request.POST.copy()
         x=Restaurant.objects.filter(pk=post["id"]).values()
-        if post["vote"]=="yes":
-            new_vote_yes=float(x[0]["vote_yes"])+1
-            old_vote_no=float(x[0]["vote_no"])
-            new_rank_pct="%.4f" % (new_vote_yes/(new_vote_yes+old_vote_no))
-            Restaurant.objects.filter(pk=post["id"]).update(vote_yes=new_vote_yes, rank_pct=new_rank_pct)
-        if post["vote"]=="no":
-            old_vote_yes=float(x[0]["vote_yes"])
-            new_vote_no=float(x[0]["vote_no"])+1
-            new_rank_pct="%.4f" % (old_vote_yes/(old_vote_yes+new_vote_no))
-            Restaurant.objects.filter(pk=post["id"]).update(vote_no=new_vote_no, rank_pct=new_rank_pct)
-
-    if content:
-        Comment.objects.create(restaurant_id=commentnameid, user_id=user.id, content=content).save()
+        if "comment" in post:
+            content=post["content"]
+            content=re.sub("<a href=.*?</a>","",content)
+            content=re.sub("<\?","",content)
+            if content:
+                Comment.objects.create(restaurant_id=post["id"], user_id=request.user.id, content=content).save()
+        if "vote" in post: 
+            if post["vote"]=="yes":
+                new_vote_yes=float(x[0]["vote_yes"])+1
+                old_vote_no=float(x[0]["vote_no"])
+                new_rank_pct="%.4f" % (new_vote_yes/(new_vote_yes+old_vote_no))
+                Restaurant.objects.filter(pk=post["id"]).update(vote_yes=new_vote_yes, rank_pct=new_rank_pct)
+            if post["vote"]=="no":
+                old_vote_yes=float(x[0]["vote_yes"])
+                new_vote_no=float(x[0]["vote_no"])+1
+                new_rank_pct="%.4f" % (old_vote_yes/(old_vote_yes+new_vote_no))
+                Restaurant.objects.filter(pk=post["id"]).update(vote_no=new_vote_no, rank_pct=new_rank_pct)
 
     return render_to_response('index.html', {'data': data, 'detail': _detail, 'comments': comments}, RequestContext(request))
 
